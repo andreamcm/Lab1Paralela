2e539ead8fe1fecd2db2f0601a48d74daf146748
ivettemar16020 <car16020@uvg.edu.gt>
2020-02-12 23:19:29 -0600

    Ambos programas

--- trapOMP2.c
+++ trapOMP2.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include <time.h>
 
 double f(double x);    /* Function we're integrating */
-void Trap(double a, double b, int n, double* global_result_p);
+double Local_trap(double a, double b, int n);
 
 int main(int argc, char* argv[]) {
    double  global_result = 0.0;  /* Store result in global_result */
@@ -12,16 +13,29 @@ int main(int argc, char* argv[]) {
    int     n;                    /* Total number of trapezoids    */
    int     thread_count;
 
+   clock_t start, end;           /*       Time Varaibles          */
+   double cpu_time_used;
+
    thread_count = strtol(argv[1], NULL, 10);
    printf("Enter a, b, and n\n");
    scanf("%lf %lf %d", &a, &b, &n);
+   start = clock();
 
-#  pragma omp parallel num_threads(thread_count) 
-   Trap(a, b, n, &global_result);
+   global_result = 0.0;
+#  pragma omp parallel num_threads(thread_count)
+   {
+      double my_result = 0.0;
+      my_result += Local_trap(a, b, n);
+#     pragma omp critical
+      global_result += my_result;
+   }
+   end = clock();
+   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
    printf("With n = %d trapezoids, our estimate\n", n);
    printf("of the integral from %f to %f = %.14e\n",
       a, b, global_result);
+   printf("Time taken: %lf \n", cpu_time_used);
    return 0;
 }  /* main */
 
@@ -48,24 +62,23 @@ double f(double x) {
  * Output arg:
  *    integral:  estimate of integral from a to b of f(x)
  */
-void Trap(double a, double b, int n, double* global_result_p) {
+double Local_trap(double a, double b, int n) {
    double  h, x, my_result;
    double  local_a, local_b;
    int  i, local_n;
    int my_rank = omp_get_thread_num();
    int thread_count = omp_get_num_threads();
 
-   h = (b-a)/n; 
-   local_n = n/thread_count;  
-   local_a = a + my_rank*local_n*h; 
-   local_b = local_a + local_n*h; 
-   my_result = (f(local_a) + f(local_b))/2.0; 
+   h = (b-a)/n;
+   local_n = n/thread_count;
+   local_a = a + my_rank*local_n*h;
+   local_b = local_a + local_n*h;
+   my_result = (f(local_a) + f(local_b))/2.0;
    for (i = 1; i <= local_n-1; i++) {
      x = local_a + i*h;
      my_result += f(x);
    }
-   my_result = my_result*h; 
+   my_result = my_result*h;
 
-#  pragma omp critical 
-   *global_result_p += my_result; 
+   return my_result;
 }  /* Trap */
